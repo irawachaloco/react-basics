9971a8189248af2b5295952f4c2516ae85ebfdcb
“irawachaloco” <irawachaloco@gmail.com>
2017-11-07 23:01:12 -0600

    adding cv and some styles

--- src/views/curriculum.tsx
+++ src/views/curriculum.tsx
@@ -1,16 +1,14 @@
 import * as React from 'react';
 
 export const CurriculumVitae = () =>
-<section>
-    <div className='flex flex_center'>
-        <div className='small-wrapper'>
-            <span>Desarrollador, diseñador y artista crónico centrado en la interacción humana.</span>
-            <span>Me gustan los proyectos que involucran creatividad, funcionalidad y usabilidad, a través de la comunicación visual.</span>
-            <span>Arte, lenguaje, aprendizaje, creatividad, comunicación son algunos de los temas que rigen mi carrera profesional.</span>
-            <span>Por ahora, me enfoco principalmente en el mundo digital.</span>
-            <span>Mantener la experiencia fluida de un usuario mientras se logran los objetivos.</span>
+    // cómo eliminar ese lint de abajo: Exceeds maximum line length of 120
+    <section>
+        <div className='flex flex_center'>
+            <div className='small-wrapper'>
+                <p>Desarrollador, diseñador y artista crónico centrado en la interacción humana. Me gustan los proyectos que involucran creatividad, funcionalidad y usabilidad a través de la expresión visual. Arte, lenguaje, comunicación y aprendizaje son algunos temas que rigen mi carrera. Por ahora, me enfoco principalmente en el mundo digital. Mantener una experiencia fluida mientras se logran las metas.</p>
+                <p>Herramientas de trabajo: mucho código, mal humor, buen gusto y algunas habilidades para manipulación de mapas de bits / vectores.</p>
+            </div>
         </div>
-    </div>
-</section>;
+    </section>;
 
 export default CurriculumVitae;
