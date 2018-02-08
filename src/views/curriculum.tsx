924ec900bb765102372f8e3600c32d7a35f29fb2
“irawachaloco” <irawachaloco@gmail.com>
2018-02-07 18:54:21 -0600

    cambio redacción texto

--- src/views/curriculum.tsx
+++ src/views/curriculum.tsx
@@ -4,14 +4,14 @@ export const CurriculumVitae = () =>
     <section>
         <div className='flex flex_center'>
             <div className='small-wrapper'>
+                <h2>irawachaloco | omar ponce</h2>
                 <p>Desarrollador, diseñador y artista crónico centrado en
-                    la interacción humana. Me gustan los proyectos que
+                    la interacción humana, asentado en la Ciudad de México. Estima los proyectos que
                     involucran creatividad, funcionalidad y usabilidad
                     a través de la expresión visual. Arte, lenguaje,
                     comunicación y aprendizaje son algunos temas que
-                    rigen mi carrera. Por ahora, me enfoco principalmente
-                    en el mundo digital. Mantener una experiencia fluida
-                    mientras se logran las metas.</p>
+                    rigen su carrera. Por ahora, enfocado principalmente
+                    en el mundo digital. Mantiene la concentración en el uso humano.</p>
                 <p>Herramientas de trabajo: mucho código, mal humor,
                     buen gusto y algunas habilidades para manipulación
                     de mapas de bits / vectores.</p>
